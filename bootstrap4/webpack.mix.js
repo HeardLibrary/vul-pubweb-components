0251a2bb9464d75e847043865d314684365d192c
Matthew Dillingham <16145372+dillinrm@users.noreply.github.com>
2022-04-26 13:39:10 -0500

    Bootstrap 4 footer

--- bootstrap4/webpack.mix.js
+++ bootstrap4/webpack.mix.js
@@ -5,12 +5,18 @@ mix.setPublicPath('dist');
 mix.less('src/bootstrap-3.4.1/less/bootstrap.less', 'css')
     .styles([
         'dist/css/bootstrap.css',
-        'src/css/vu-main-2019.css',
         'src/css/vu-brandbar.css', 
-        'src/css/vul-header-bs3.css'
+        'src/css/vu-main-2019-header.css',
+        'src/css/vul-header.css'
     ], 'dist/css/vul-pw-header.css')
+    .styles([
+        'dist/css/bootstrap.css',
+        'src/css/vu-main-2019-footer.css',
+        'src/css/vul-footer.css'
+    ], 'dist/css/vul-pw-footer.css')
     .js([
         'src/js/vul-brandbar.js', 
         'src/js/vul-header.js', 
         'src/js/vul-navigation.js'
-    ], 'dist/js/vul-pw-header.js');
+    ], 'dist/js/vul-pw-header.js')
+    .copy('src/js/vul-footer.js', 'dist/js/vul-pw-footer.js');
