49a192d10ad0ca0eed279db44535af45ba57617f
Matthew Dillingham <16145372+dillinrm@users.noreply.github.com>
2022-04-26 13:16:20 -0500

    Bundle and distribute footer files

--- bootstrap3/webpack.mix.js
+++ bootstrap3/webpack.mix.js
@@ -9,8 +9,14 @@ mix.less('src/bootstrap-3.4.1/less/bootstrap.less', 'css')
         'src/css/vu-main-2019-header.css', 
         'src/css/vul-header.css'
     ], 'dist/css/vul-pw-header.css')
+    .styles([
+        'dist/css/bootstrap.css',
+        'src/css/vu-main-2019-footer.css',
+        'src/css/vul-footer.css'
+    ], 'dist/css/vul-pw-footer.css')
     .js([
         'src/js/vul-brandbar.js', 
         'src/js/vul-header.js', 
         'src/js/vul-navigation.js'
-    ], 'dist/js/vul-pw-header.js');
+    ], 'dist/js/vul-pw-header.js')
+    .copy('src/js/vul-footer.js', 'dist/js/vul-pw-footer.js');
