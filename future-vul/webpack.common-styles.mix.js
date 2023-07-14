9765ff3ade58e0ab643382a228842becb71e927c
Matthew Dillingham <16145372+dillinrm@users.noreply.github.com>
2023-07-14 15:20:03 -0500

    Rename file

--- future-vul/webpack.common-styles.mix.js
+++
@@ -1,3 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.styles(['src/css/vul-pwc-common-styles.css'], 'dist/css/vul-pwc-common-styles.css');
---
+++ future-vul/webpack.fonts.mix.js
@@ -0,0 +1,3 @@
+let mix = require('laravel-mix');
+
+mix.styles(['src/css/vul-pwc-fonts.css'], 'dist/css/vul-pwc-fonts.css');
